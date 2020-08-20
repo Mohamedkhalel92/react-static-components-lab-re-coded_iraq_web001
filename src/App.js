b2ef5d2fcd6ff423872cac15279476e0aebfa649
mohamed khalel <mohamed.khalel.1992.1992@gmail.com>
2020-08-20 14:23:38 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import CatComponent from './CatComponent.js';
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent.js' 
 class App extends Component {
 	render() {
 		// your code in the return statement below!
@@ -8,6 +9,7 @@ class App extends Component {
 				<CatComponent />
 				<GraceHopperQuoteComponent />
 				{/* one more component missing */}
+				
 			</div>
 		);
 	}
